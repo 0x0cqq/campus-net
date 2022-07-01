7debbba208e17ac5dba82ec9145f4cd30b4af4e6
ChenQiqian <cqqqwq@outlook.com>
2022-07-01 19:08:24 +0800

    update code, add status page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Info name="cqqthu20"></Info>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from './components/HelloWorld.vue'
+import Info from './components/Info.vue'
 
 @Options({
   components: {
-    HelloWorld
+    HelloWorld,
+    Info
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  data () {
+    return {
+      name: 'cqqthu20'
+    }
+  }
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 100px;
+}
+body {
+  background: #E6E6E6;
 }
 </style>
---
+++ src/components/Info.vue
@@ -0,0 +1,323 @@
+<template>
+  <div id="greeting">
+    <div id="welcome-text">
+      <p id="laugh-welcome">:P</p>
+      <p id="chinese-welcome">欢迎,</p>
+      <p id="english-welcome">Welcome,</p>
+    </div>
+    <div id="greeting-triangle"></div>
+  </div>
+  <div id="info-block">
+    <h2 id="user-name">{{ name }}</h2>
+    <div style="height:22px"></div>
+    <div id="info-content">
+      <div class="info-block" id="connect-info">
+        <div class="label-text">
+          <p>已连接</p>
+          <p class="english">Duration</p>
+        </div>
+        <div class="info-block-content" id="connect-time">
+          <p id="display-connect-time">{{hour}}:{{minute}}:{{second}}</p>
+        </div>
+      </div>
+      <div class="info-block" id="usage-info">
+        <div class="label-text">
+          <p>已用流量</p>
+          <p class="english">Usage</p>
+        </div>
+        <div class="info-block-content" id="usage">
+          <div id="usage-volume">
+            <p>&nbsp;&nbsp;&nbsp;</p>
+            <p>&nbsp;&nbsp;&nbsp;</p>
+            <p>&nbsp;50</p>
+            <p>&nbsp;75</p>
+            <p>100</p>
+            <p>125</p>
+          </div>
+          <div id="usage-bar">
+            <div id="usage-bar-fill" :style="{width : usageNumberLength}"></div>
+            <div id="usage-number">{{usageNumberText}}</div>
+            <div id="usage-bar-50"></div>
+            <div id="usage-bar-50-75"></div>
+            <div id="usage-bar-75-100"></div>
+            <div id="usage-bar-100-125"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="bar">
+      <div id="corner"></div>
+      <button id="disconnect-button">
+        <p id="chinese-disconnect">断开连接</p>
+        <p class="english" id="english-disconnect">Disconnect</p>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+    name: String
+  }
+})
+export default class Info extends Vue {
+  name!: string
+  hour!: string
+  minute!: string
+  second!: string
+  startDate!: Date
+  usageNumber!: number // record as bytes
+  data () {
+    return {
+      hour: '00',
+      minute: '00',
+      second: '00',
+      startDate: new Date(),
+      usageNumber: 10000000000
+    }
+  }
+
+  mounted () {
+    this.startDate = new Date()
+    setInterval(() => {
+      this.updateTime()
+    }, 500)
+  }
+
+  public updateTime () : void {
+    const now = new Date()
+    const diff = now.getTime() - this.startDate.getTime()
+    const hour = Math.floor(diff / (1000 * 60 * 60))
+    const minute = Math.floor(diff / (1000 * 60)) % 60
+    const second = Math.floor(diff / 1000) % 60
+    this.hour = hour.toString().padStart(2, '0')
+    this.minute = minute.toString().padStart(2, '0')
+    this.second = second.toString().padStart(2, '0')
+  }
+
+  get usageNumberLength () : string {
+    const percentage = Math.ceil(Math.min(this.usageNumber / (125 * 1024 * 1024 * 1024), 1) * 100)
+    return `${percentage}%`
+  }
+
+  get usageNumberText () : string {
+    // as bytes
+    if (this.usageNumber < 1024) {
+      return `${this.usageNumber}B`
+    } else if (this.usageNumber < 1024 * 1024) {
+      return `${(this.usageNumber / 1024).toFixed(2)}KB`
+    } else if (this.usageNumber < 1024 * 1024 * 1024) {
+      return `${(this.usageNumber / (1024 * 1024)).toFixed(2)}MB`
+    } else {
+      return `${(this.usageNumber / (1024 * 1024 * 1024)).toFixed(2)}GB`
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0%;
+  padding: 0%;
+  border: 0;
+}
+#info-block {
+  position: relative;
+  left: 128px;
+  top: 32px;
+  width: 352px;
+  height: 208px;
+  padding: 32px;
+  background-color: #f2f2f2;
+}
+#user-name {
+  text-align: start;
+  color: #E64E2E;
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 34px;
+  font-weight: lighter;
+}
+#display-connect-time {
+  color: #Ec6677;
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 28px;
+  font-weight: lighter;
+  margin: 0px;
+}
+#usage-bar {
+  position: relative;
+  width: 280px;
+  height: 32px;
+}
+#usage-volume {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 280px;
+}
+#usage-volume p {
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 13px;
+  text-align: right;
+  color: #969696;
+  margin: 0px;
+}
+#usage-bar-50 {
+  float: left;
+  position: absolute;
+  left: 0%;
+  width: 40%;
+  height: 100%;
+  z-index: 1;
+  background-color: #D8D8D8;
+}
+#usage-bar-50-75 {
+  float: left;
+  position: absolute;
+  left: 40%;
+  width: 20%;
+  height: 100%;
+  z-index: 1;
+  background-color: #E7E3E4;
+}
+#usage-bar-75-100 {
+  float: left;
+  position: absolute;
+  left: 60%;
+  width: 20%;
+  height: 100%;
+  z-index: 1;
+  background-color: #DBD5D7;
+}
+#usage-bar-100-125 {
+  float: left;
+  position: absolute;
+  left: 80%;
+  width: 20%;
+  height: 100%;
+  z-index: 1;
+  background-color: #CFC8CB;
+}
+#usage-number{
+  float: left;
+  position: absolute;
+  left: 0%;
+  font-size : 24px;
+  color: #5C5C5C;
+  text-align: left;
+  z-index: 3;
+  margin-left: 10px;
+}
+#usage-bar-fill{
+  float: left;
+  position: absolute;
+  left: 0%;
+  height: 100%;
+  background-color: #E64E2E;
+  z-index: 2;
+}
+#bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: -16px;
+  right: -16px;
+  width: 208px;
+  height: 80px;
+  background-color: #FFFFFF;
+  box-shadow: 0 0 8px rgba(0,0,0,0.1);
+}
+#disconnect-button {
+  width: 174px;
+  height: 46px;
+  background-color: #c0bdcc;
+  border: #B2AABE solid 1px;
+}
+#chinese-disconnect {
+  font-family: "Dengxian";
+  font-weight: bold;
+  color: #444444;
+  font-size: 22px;
+  margin-top: 0px;
+}
+#english-disconnect {
+  font-size: 20px;
+  margin-top: -3px;
+  color:#555555;
+  font-family: "Dengxian"
+}
+.info-block-content {
+  margin: 0px 10px;
+}
+.info-block {
+  display: flex;
+  flex-direction: row;
+}
+.label-text {
+  color: #303030;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+.label-text p {
+  font-size: 12px;
+  /* float: left; */
+  text-align: right;
+  margin: 0%;
+}
+.english {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+#corner{
+  position: absolute;
+  top: -16px;
+  right: 0px;
+  border-color: transparent #CCCCCC;
+  border-width: 16px 0 0 16px;
+  border-style: solid;
+}
+#greeting {
+  position: absolute;
+  width: 512px;
+  height: 224px;
+  background-color: #E64E2E;
+  z-index: -1;
+  box-shadow: 0 0 8px rgb(0 0 0 / 10%);
+  color: #FFFFFF;
+}
+#greeting-triangle {
+  position: absolute;
+  top: 224px;
+  left: 32px;
+  border-color: #E64E2E transparent ;
+  border-width: 48px 48px 0 0;
+  border-style: solid;
+}
+#laugh-welcome {
+  margin-top: -15px;
+  font-size: 80px;
+  font-weight: lighter;
+}
+#chinese-welcome {
+  margin-top: 20px;
+  font-family: "Dengxian";
+  font-weight: lighter;
+  font-size: 30px;
+  margin-top: 0px;
+}
+#english-welcome {
+  margin-top: 15px;
+  font-size: 20px;
+  color: rgba(255,255, 255,0.9);
+  font-family: "Dengxian";
+  font-weight: lighter
+}
+#welcome-text {
+  margin: 32px 0 0 32px;
+}
+</style>
